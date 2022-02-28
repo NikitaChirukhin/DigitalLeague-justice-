4df6d30df41eac14743f46ca4b5ffa6b2dbef311
Nikita <45002477+NikitaChirukhin@users.noreply.github.com>
2022-02-28 19:17:59 +0300

    Add files via upload

---
+++ task3/task3/ValidationView.swift
@@ -0,0 +1,92 @@
+//
+//  NameValidationView.swift
+//  task3
+//
+//  Created by Никита Чирухин on 22.02.2022.
+//
+
+import UIKit
+
+class ValidationView: UIView {
+    
+    weak var delegate: NameValidationViewDelegate?
+    
+    lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .systemBackground
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var validationResultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Waiting..."
+        return label
+    }()
+    
+    lazy var validationButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Validate", for: .normal)
+        button.setTitle("STOP", for: .highlighted)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .systemMint
+        button.layer.cornerRadius = 4
+        button.addTarget(self, action: #selector(tapeOnButton), for: .touchUpInside)
+        return button
+    }()
+    
+    init(validationTargetName: String) {
+        super.init(frame: .zero)
+        
+        textField.placeholder = validationTargetName
+
+        setUpView()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: - ValidationView Private methods
+
+extension ValidationView {
+    @objc private func tapeOnButton() {
+        let isValid = delegate?.didTapOnValidationButton(validationText: textField.text ?? "", validationTarget: textField.placeholder ?? "")
+        if isValid != nil && isValid == true {
+            validationResultLabel.text = "Success.."
+            validationResultLabel.textColor = .systemGreen
+        } else {
+            validationResultLabel.text = "Wrong name.."
+            validationResultLabel.textColor = .systemRed
+        }
+
+    }
+    
+    private func setUpView() {
+        addSubview(textField)
+        addSubview(validationButton)
+        addSubview(validationResultLabel)
+    }
+    
+    private func setUpConstraint() {
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            
+            validationButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 16),
+            validationButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            validationButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            
+            validationResultLabel.topAnchor.constraint(equalTo: validationButton.bottomAnchor, constant: 16),
+            validationResultLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            validationResultLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            validationResultLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+        ])
+    }
+}
---
+++ task3/task3/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  task3
+//
+//  Created by Никита Чирухин on 22.02.2022.
+//
+
+import UIKit
+
+protocol NameValidationViewDelegate: AnyObject {
+    func didTapOnValidationButton(validationText: String, validationTarget: String) -> Bool
+}
+
+class ViewController: UIViewController {
+    lazy var nameValidationView: ValidationView = {
+        let view = ValidationView(validationTargetName: "FullName")
+        view.delegate = self
+        view.backgroundColor = .secondarySystemBackground
+        view.layer.cornerRadius = 12
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var emailValidationView: ValidationView = {
+        let view = ValidationView(validationTargetName: "Email")
+        view.delegate = self
+        view.backgroundColor = .secondarySystemBackground
+        view.layer.cornerRadius = 12
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        setupConstraints()
+    }
+}
+
+//MARK: - ViewController Private methods
+
+extension ViewController: NameValidationViewDelegate {
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(nameValidationView)
+        view.addSubview(emailValidationView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            nameValidationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            nameValidationView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            nameValidationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameValidationView.heightAnchor.constraint(equalToConstant: 150),
+            
+            emailValidationView.topAnchor.constraint(equalTo: nameValidationView.bottomAnchor, constant: 50),
+            emailValidationView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            emailValidationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailValidationView.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    func didTapOnValidationButton(validationText: String, validationTarget: String) -> Bool {
+        
+        var isValid = false
+        
+        if validationTarget == "FullName" {
+            let regExString = "[A-Za-zА-ЯЁа-яё-]{2,}+\\s{1}+[A-Za-zА-ЯЁа-яё-]{2,}"
+            let predicate = NSPredicate(format: "SELF MATCHES[c] %@", regExString)
+            isValid = predicate.evaluate(with: validationText)
+        }
+        if validationTarget == "Email" {
+            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            let predicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+            isValid = predicate.evaluate(with: validationText)
+        }
+        
+        return isValid
+    }
+}
